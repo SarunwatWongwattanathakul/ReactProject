95cad6d001c0d546cd1d2b7671be1ec384c5f730
SarunwatWongwattanathakul <miczuka2545@gmail.com>
2022-06-29 10:58:42 +0700

    forth commit

--- App.js
+++ App.js
@@ -1,29 +1,21 @@
-import { StyleSheet, Text, View,Button } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Logo from './components/Logo'
+import User from './components/User'
+import { Button } from 'react-native-web'
 
 const App = () => {
-  const showData =() =>{
-    alert("Hello Button")
-  }
-
   return (
-    <View  style={styles.container}>
-       <Text>Button Example</Text>
-       <Button
-        title = 'Click Me'
-        onPress={showData}
-        color = 'skyblue'
+    <View>
+      <Logo/>
+      <Button
+        title = 'Click me'
       />
+      <User/>
     </View>
   )
 }
 
-const styles = StyleSheet.create({
-  container:{
-   flex: 1,
-   alignItems: 'center',
-   justifyContent: 'center'
-  }
-});
-
 export default App
+
+const styles = StyleSheet.create({})
