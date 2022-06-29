d891a4827da37fbf39239f925280f3cf7cf8b82e
SarunwatWongwattanathakul <miczuka2545@gmail.com>
2022-06-29 10:35:34 +0700

    third commit

--- App.js
+++ App.js
@@ -1,30 +1,29 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { StyleSheet, Text, View,Button } from 'react-native'
 import React from 'react'
-import Logo from './components/Logo';
-import Stysheet from './components/Stysheet';
 
 const App = () => {
+  const showData =() =>{
+    alert("Hello Button")
+  }
+
   return (
-    <View style={styles.container}>
-      {/* <Text style={[styles.title,styles.warning]}>Hello React Native</Text> */}
-      {/* <Logo /> */}
-      <Stysheet/>
+    <View  style={styles.container}>
+       <Text>Button Example</Text>
+       <Button
+        title = 'Click Me'
+        onPress={showData}
+        color = 'skyblue'
+      />
     </View>
   )
 }
 
-export default App
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignContent: 'center'
-  },
-  title: {
-    fontSize: 20,
-  },
-  warning: {
-    color: 'red'
+  container:{
+   flex: 1,
+   alignItems: 'center',
+   justifyContent: 'center'
   }
 });
+
+export default App
--- components/Logo.js
+++ components/Logo.js
@@ -1,16 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 const Logo = () => {
-    const TextLogo = "Thai-Nichi";
+    const textLogo = "Thai-Nihi";
+    const isTH = false;
     return (
         <View>
             <Text style={styles.TextLogo}>TNI</Text>
             <Text>{textLogo}</Text>
+            {/* {
+                isTH && <Text>ภาษาไทย</Text>
+            } */}
+            {/* use if/else or conditonal operator */}
+            {
+                isTH
+                ?(<Text>ภาษาไทย</Text>)
+                :(<Text>ภาษาอังกฤษ</Text>)
+            }
+
         </View>
     )
 }
+
 export default Logo
+
 const styles = StyleSheet.create({
     TextLogo:{
         color:"blue",
