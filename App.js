38b63877ec09778d255f66be4fb047c217a611b5
SarunwatWongwattanathakul <miczuka2545@gmail.com>
2022-07-27 09:10:37 +0700

    seven commit practice

--- App.js
+++ App.js
@@ -13,10 +13,11 @@ import AlertComponent from './components/AlertComponent'
 import ImageWithTextInput from './components/ImageWithTextInput'
 import ButtonExample from './components/ButtonExample'
 import Touchable_Example from './components/Touchable_Example'
+import TouchablePractice from './components/TouchablePractice'
 
 const App = () => {
   return (
-    <View style={styles.container} >
+    <View  >
       {/*<Logo/>*/}
       {/*<Button title = 'Click me'/>*/}
       {/*<User/>*/}
@@ -29,7 +30,8 @@ const App = () => {
       {/*<AlertComponent/>*/}
       {/*<ImageWithTextInput/>*/}
       {/*<ButtonExample/> */}
-      <Touchable_Example/>
+      {/*<Touchable_Example/> */}
+      <TouchablePractice/>
     </View>
   )
 }
